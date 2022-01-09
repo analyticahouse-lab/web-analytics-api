9d7ae9b1edaaab16d3417486c8d832288080d18e
Berkay <34893128+raufsamestone@users.noreply.github.com>
2022-01-09 21:08:09 +0300

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,3 +1,7 @@
 var cookieLib = document.createElement('script');
 cookieLib.src = 'https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js';
 document.head.appendChild(cookieLib);
+
+
+var cookieName = 'CM'
+Cookies.set(cookieName, userData.cd_user_id, { sameSite: 'Lax', secure: true, expires: 7})  
